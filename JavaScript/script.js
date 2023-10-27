f66f47c58ae2a7e8fc86fc1d942c91046486c9c8
Seong Hyun Kang <ruanelroem@gmail.com>
2023-10-27 14:45:57 +0900

    JS Practice_02

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,25 @@
-function myExternalAlert(){
+function myExternalAlert() {
     alert('world!!!!!!!!!!!');
+}
+
+function validateAndPrint() {
+    const numberInput = document.getElementById('numberInput');
+    const inputValue = numberInput.value;
+    const number = parseInt(inputValue);
+
+    if (isNaN(number)) {
+        alert('숫자를 입력해 주세요');
+        return;
+    }
+
+    if (number < 1 || number > 100) {
+        alert('1이상 100 이하를 넣어주세요');
+        return;
+    }
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+
+    alert('완료되었습니다.');
 }
