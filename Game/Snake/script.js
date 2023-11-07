4fa4074475811960645225f4b241c74badb727d1
Seong Hyun Kang <ruanelroem@gmail.com>
2023-11-07 13:47:22 +0900

    minor issue fix
    - relpace food spawn location when it overlaps with segments
    - snake control logic managed

--- Game/Snake/script.js
+++ Game/Snake/script.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
     const gameArea = $('#game-area');
     const gameWidth = gameArea.width();
     const gameHeight = gameArea.height();
     const snakeSize = 20;
     let snake = [];
     let direction = { x: 0, y: 0 };
+    let newDirection = null;
+    let directionChanged = false;
     let foodPosition = { top: 0, left: 0 };
     let score = 0;
     let gameInterval;
@@ -13,17 +15,27 @@ $(document).ready(function(){
     let gameStarted = false;
 
     function placeFood() {
-        foodPosition.top = Math.round(Math.random() * (gameHeight - snakeSize) / snakeSize) * snakeSize;
-        foodPosition.left = Math.round(Math.random() * (gameWidth - snakeSize) / snakeSize) * snakeSize;
+        let foodPlaced = false;
+        while (!foodPlaced) {
+            foodPosition.top = Math.round(Math.random() * (gameHeight - snakeSize) / snakeSize) * snakeSize;
+            foodPosition.left = Math.round(Math.random() * (gameWidth - snakeSize) / snakeSize) * snakeSize;
+    
+            let collision = snake.some(segment => {
+                return foodPosition.top === segment.top && foodPosition.left === segment.left;
+            });
+    
+            if (!collision) {
+                foodPlaced = true;
+            }
+        }
         $('#food').css(foodPosition);
     }
 
     function drawSnake() {
-        $('#snake').empty(); // Remove the previous snake parts
+        $('#snake').empty();
         snake.forEach((segment, index) => {
-            const segmentDiv = $('<div></div>');
-            segmentDiv.addClass('snake-segment');
-            if (index === 0) { // If it's the first segment, it's the head
+            const segmentDiv = $('<div></div>').addClass('snake-segment');
+            if (index === 0) {
                 segmentDiv.addClass('snake-head');
             }
             segmentDiv.css({ top: segment.top + 'px', left: segment.left + 'px' });
@@ -32,13 +44,9 @@ $(document).ready(function(){
     }
 
     function moveSnake() {
-        const newHead = {
-            top: snake[0].top + (direction.y * snakeSize),
-            left: snake[0].left + (direction.x * snakeSize)
-        };
-
+        const newHead = { top: snake[0].top + (direction.y * snakeSize), left: snake[0].left + (direction.x * snakeSize) };
         snake.unshift(newHead);
-        if (snake[0].top === foodPosition.top && snake[0].left === foodPosition.left) {
+        if (newHead.top === foodPosition.top && newHead.left === foodPosition.left) {
             score += 10;
             placeFood();
         } else {
@@ -46,9 +54,11 @@ $(document).ready(function(){
         }
         if (checkCollision(newHead)) {
             resetGame();
+        } else {
+            drawSnake();
+            $('#score').text(score);
         }
-        drawSnake();
-        $('#score').text(score);
+        updateDirection();
     }
 
     function checkCollision(head) {
@@ -66,11 +76,10 @@ $(document).ready(function(){
     function resetGame() {
         clearInterval(gameInterval);
         clearInterval(timerInterval);
-        snake = [{
-            top: Math.floor((gameHeight - snakeSize) / 2),
-            left: Math.floor((gameWidth - snakeSize) / 2)
-        }];
+        snake = [{ top: Math.floor((gameHeight - snakeSize) / 2), left: Math.floor((gameWidth - snakeSize) / 2) }];
         direction = { x: 0, y: 0 };
+        newDirection = null;
+        directionChanged = false;
         score = 0;
         timeSurvived = 0;
         gameStarted = false;
@@ -80,11 +89,15 @@ $(document).ready(function(){
         drawSnake();
     }
 
-    $(document).keydown(function(e){
-        if (e.which >= 37 && e.which <= 40) {
-            e.preventDefault();
+    function updateDirection() {
+        if (newDirection !== null) {
+            direction = newDirection;
+            newDirection = null;
+            directionChanged = false;
         }
+    }
 
+    $(document).keydown(function(e) {
         if (!gameStarted) {
             gameStarted = true;
             gameInterval = setInterval(moveSnake, 100);
@@ -93,15 +106,29 @@ $(document).ready(function(){
                 $('#time').text(timeSurvived);
             }, 1000);
         }
-        if (e.which === 37 && direction.x !== 1) {
-            direction = { x: -1, y: 0 };
-        } else if (e.which === 38 && direction.y !== 1) {
-            direction = { x: 0, y: -1 };
-        } else if (e.which === 39 && direction.x !== -1) {
-            direction = { x: 1, y: 0 };
-        } else if (e.which === 40 && direction.y !== -1) {
-            direction = { x: 0, y: 1 };
+
+        if (directionChanged) {
+            return;
         }
+
+        const newDir = { x: direction.x, y: direction.y };
+
+        if (e.which === 37 && direction.x !== 1) {
+            newDir.x = -1; newDir.y = 0;
+        } else if (e.which === 38 && direction.y !== 1) {
+            newDir.x = 0; newDir.y = -1;
+        } else if (e.which === 39 && direction.x !== -1) {
+            newDir.x = 1; newDir.y = 0;
+        } else if (e.which === 40 && direction.y !== -1) {
+            newDir.x = 0; newDir.y = 1;
+        }
+
+        if (direction.x !== newDir.x || direction.y !== newDir.y) {
+            newDirection = newDir;
+            directionChanged = true;
+        }
+
+        e.preventDefault();
     });
 
     resetGame();
